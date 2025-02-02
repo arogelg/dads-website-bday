79f57b546e4495d45584b4e7e26b3b96cc8d51dc
Alejandro Rogel <alex_rogelgarcia1@baylor.edu>
2025-02-02 16:41:11 -0600

    Making changes to the animation of index

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     setTimeout(() => {
-        document.querySelector(".text-overlay").style.opacity = "1";
-    }, 1000);
+        document.querySelector(".big-text").style.opacity = "1";
+        document.querySelector(".small-text").style.opacity = "1";
+        document.querySelector(".birthday-image").style.opacity = "1";
+    }, 500);
 });
